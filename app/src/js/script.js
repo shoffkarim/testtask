5c9fe4824528fd3b05dd6c5defbfda1c5f3ba171
shoffkarim <k4r1mch1k@gmail.com>
2021-02-10 15:31:33 +0500

    add dropdown and change script

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,5 +1,3 @@
 // STYLES=============================================================================
 import '../sass/libs/bootstrap.min.css';
 import '../sass/style.sass';
-
-import './libs/bootstrap.min.js';
